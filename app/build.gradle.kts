cb3f2a78e6b4a29551d834370bc758fc8855e37e
tri <vodaiminhtri@gmail.com>
2025-11-13 18:12:08 +0700

    xong product

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,6 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.github.bumptech.glide:glide:5.0.5")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
 }
